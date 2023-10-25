fd66845d90a6ae12caf2f3e960f434e47b118f2f
Lokesh Rangineni <lokeshforjava@gmail.com>
2023-10-25 15:56:13 -0400

    implemented the hashing in the folder path to avoid the folder collision. cleaned up some code.

--- plugins/rhda-backend/src/service/exhortHandler.ts
+++ plugins/rhda-backend/src/service/exhortHandler.ts
@@ -1,36 +1,37 @@
 import {Request, Response} from 'express';
 import { getRootLogger } from '@backstage/backend-common';
 import childProcess from 'node:child_process';
-import * as fs from "fs";
+import * as clone from 'git-clone/promise';
+import * as crypto from "crypto";
+import { recreateFolder, deleteFolderIfItExists } from './fileUtil';
 
 export const rhdaHandler = (req: Request, res: Response) => {
     const logger = getRootLogger();
-    const clone = require('git-clone/promise');
     const repo = `https://github.com/${req.query.repositorySlug}.git`
-    // TODO: make this path unique to avoid conflict. implement hashing.
-    const gitSourceDir = `./git-source`;
-    // delete the folder if it exists already.
-    fs.rmSync(gitSourceDir, { recursive: true, force: true });
-
-    const  pomFileDirectory = `${gitSourceDir}/${req.query.manifestFilePath}`
-    const execCommand = `NODE_NO_WARNINGS=1 exhort-javascript-api stack ${pomFileDirectory} --summary`;
-
+    const gitSourceDir = `./git-source/${crypto.randomBytes(20).toString('hex')}`;
+    recreateFolder(gitSourceDir);
     clone(repo, gitSourceDir).then(async () => {
         logger.info(`cloned the repo=[${repo}] successfully to: ${gitSourceDir}`);
+        const  pomFileDirectory = `${gitSourceDir}/${req.query.manifestFilePath}`
+        const execCommand = `NODE_NO_WARNINGS=1 exhort-javascript-api stack ${pomFileDirectory} --summary`;
         childProcess.exec(execCommand, (error, stdout, stderr) => {
             if (error) {
-                console.log(`error: ${error.message}`);
-                return;
+                logger.error(`error: ${error.message}`);
+                throw error;
             }
             if (stderr) {
-                logger.error(`FAILED to RUN the scan on the repo=[${repo}] to ${gitSourceDir}, with error=${stderr}`);
-                console.log(`stderr: ${stderr}`);
-                return;
+                const errorMessage = `FAILED to RUN the scan on the repo=[${repo}] to ${gitSourceDir}, with error=${stderr}`;
+                logger.error(errorMessage);
+                throw new Error(errorMessage);
             }
+            // delete the folder after the report.
+            deleteFolderIfItExists(gitSourceDir)
             res.json(JSON.parse(stdout));
         })
     }).catch((error:any) => {
         logger.error(`FAILED to clone the repo=[${repo}] to ${gitSourceDir}, with error=${error}`);
+        // delete the folder after exception.
+        deleteFolderIfItExists(gitSourceDir)
         throw error;
     });
 };
@@ -38,36 +39,35 @@ export const rhdaHandler = (req: Request, res: Response) => {
 
 export const rhdaHtmlHandler = (req: Request, res: Response) => {
     const logger = getRootLogger();
-    const clone = require('git-clone/promise');
     const repo = `https://github.com/${req.query.repositorySlug}.git`
-    // TODO: make this path unique to avoid conflict. implement hashing.
-    const gitSourceDir = `./git-source`;
-    // delete the folder if it exists already.
-    fs.rmSync(gitSourceDir, { recursive: true, force: true });
-
-    const  pomFileDirectory = `${gitSourceDir}/${req.query.manifestFilePath}`
-    const htmlResponsePath = "/tmp/htmlResponse.html"
-    const execCommand = `NODE_NO_WARNINGS=1 exhort-javascript-api stack ${pomFileDirectory} --html > ${htmlResponsePath}`;
-
+    const gitSourceDir = `./git-source/${crypto.randomBytes(20).toString('hex')}`;
+    recreateFolder(gitSourceDir);
     clone(repo, gitSourceDir).then(async () => {
         logger.info(`cloned the repo=[${repo}] successfully to: ${gitSourceDir}`);
+        const  pomFileDirectory = `${gitSourceDir}/${req.query.manifestFilePath}`
+        const htmlResponsePath = "/tmp/htmlResponse.html"
+        const execCommand = `NODE_NO_WARNINGS=1 exhort-javascript-api stack ${pomFileDirectory} --html > ${htmlResponsePath}`;
         childProcess.exec(execCommand, {maxBuffer: 1024 * 1500},(error, stdout, stderr) => {
             if (error) {
-                console.log(`error: ${error.message}`);
-                return;
+                logger.error(`error: ${error.message}`);
+                throw error;
             }
             if (stderr) {
-                logger.error(`FAILED to RUN the scan on the repo=[${repo}] to ${gitSourceDir}, with error=${stderr}`);
-                console.log(`stderr: ${stderr}`);
-                return;
+                const errorMessage = `FAILED to RUN the scan on the repo=[${repo}] to ${gitSourceDir}, with error=${stderr}`;
+                logger.error(errorMessage);
+                throw new Error(errorMessage);
             }
             res.setHeader('Content-disposition', 'attachment; filename=RHDA-generated-html-report.html');
-            console.log("html response is generated to the file on server. Going to send the response.");
+            logger.info("html response is generated to the file on server. Going to send the response.");
+            // delete the folder after the report.
+            deleteFolderIfItExists(gitSourceDir)
             res.download(`${htmlResponsePath}`);
-            // res.sendFile('/Users/lrangine/Documents/Src/appeng-backstage/packages/backend/htmlResponse.html');
         })
     }).catch((error:any) => {
         logger.error(`FAILED to clone the repo=[${repo}] to ${gitSourceDir}, with error=${error}`);
+        // delete the folder after the report.
+        deleteFolderIfItExists(gitSourceDir)
+        throw error;
     });
 };
 
